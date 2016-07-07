337f8cb621aefd47e231db5eed432eb064e1509a
VulcanoAhab <vulcano.ahab@gmail.com>
2016-07-07 13:46:18 -0300

    Action and post action fixes

--- grabbers/utils/html.py
+++ grabbers/utils/html.py
@@ -19,13 +19,23 @@ class Grabis:
     for now just a simple wrappper
     '''
     @staticmethod
-    def _exData(field_name, htmlElement, attrs):
+    def _exData(htmlElement, attrs):
         '''
         '''
         datum=[]
         for ats in attrs:
-            if ats == 'text':
-                datum.append(htmlElement.text)
+            if ats == 'text_content':
+                content=htmlElement.text
+                if not content:
+                    content=htmlElement.text_content()
+                datum.append(content)
+            elif ats == 'iframe_source':
+                print('IN FRAME SOURCE--------', htmlElement.attrib)
+                src=None
+                for possible in ['src','data-src','href']:
+                    src=htmlElement.attrib.get(possible)
+                    if src:break
+                datum.append(src)
             else:
                 datum.append(htmlElement.attrib.get(ats))
         return datum
@@ -36,7 +46,8 @@ class Grabis:
         '''
         data_container=[dict() for _ in elements]
         for n,value in enumerate(elements):
-            data=Grabis._exData(field, value, attrs)
+            print('[+] Loadis field [{}], value[{}], attrs[{}]'.format(field, value, attrs))
+            data=Grabis._exData(value, attrs)
             data_container[n].update({field:data})
         return data_container
 
@@ -45,7 +56,10 @@ class Grabis:
         '''
         '''
         #wait for element
-        browser.wait_for_element(selector, eltype=eltype)
+        try:
+            browser.wait_for_element(selector, eltype=eltype)
+        except:
+            return None
         source=browser.get_page_source()
         return html.fromstring(source)
 
@@ -71,6 +85,8 @@ class Grabis:
     def set_page_object(self, page_object):
         '''
         '''
+        if page_object is None:
+            raise Exception('[-] Fail to load page_object')
         self._page_object=page_object
 
     def get_data(self, field_name, attrs={}, pure_elements=False):
@@ -85,11 +101,25 @@ class Grabis:
         fn=getattr(self._page_object, self._eltype)
         self.data=fn(self._selector)
 
-    def action(self, action_type, browser, element_index):
+    def action(self, action_type, browser, element_index,
+                                post_action=None, job=None):
         '''
         '''
         els=browser.browser.find_elements_by_xpath(self._selector) #shoulb be in browser api
-        getattr(els[element_index], action_type)
+        if element_index >=0:
+            targets=[els[element_index]]
+        else:
+            targets=els
+        for target in targets:
+            getattr(target, action_type)()
+            if post_action:
+                if not job:
+                    raise Exception('[-] Jobs is required for post action')
+                ps=Pythoness()
+                ps.set_job(job)
+                ps.set_grabber(post_action)
+                ps.session(browser)
+                ps.save_data()
 
 ## ---  extract page data
 class Pythoness:
@@ -124,60 +154,57 @@ class Pythoness:
         field_name=mapper_dict['name']
         selector=mapper_dict['selector']
         page_object=Grabis.load_page(browser, selector)
-        gb=Grabis()
-        gb.set_selector(selector)
-        gb.set_page_object(page_object)
-        gb.grab()
-        data=gb.get_data(field_name, pure_elements=True)
-        return data
-
+        try:
+            gb=Grabis()
+            gb.set_selector(selector)
+            gb.set_page_object(page_object)
+            gb.grab()
+            data=gb.get_data(field_name, pure_elements=True)
+            return data
+        except Exception as e:
+            print('[-] Fail to build map', e)
+            return []
 
     @classmethod
     def session(cls, browser, element_index=-1):
         '''
         '''
         # set base values
-        elements_lists=[]
-
-
         if 'target' in cls._conf:
-
             selector=cls._conf['target']['selector']
             page_object=Grabis.load_page(browser,selector)
-
-            gb=Grabis()
-            gb.set_selector(selector)
-            gb.set_page_object(page_object)
-
+            try:
+                gb=Grabis()
+                gb.set_selector(selector)
+                gb.set_page_object(page_object)
+            except Exception as e:
+                fd=open('/Users/poisonGirl/Desktop/{}'.format('__'.join(browser.browser.current_url.split('/')[-2:])), 'w')
+                fd.write(browser.browser.page_source)
+                fd.close()
+                print('[-] Fail to load conf in Grabis', e)
+                return
             if 'element_action' in cls._conf:
-                gb.action(cls._conf['element_action'],
-                          browser, element_index)
+                post_action=cls._conf['post_action']
+                gb.action(cls._conf['element_action'], browser,
+                          element_index, post_action=post_action,
+                          job=cls._job)
             if 'extractors' in cls._conf:
                 field_name=cls._conf['target']['name']
                 attrs=cls._conf['extractors']
                 gb.grab()
                 data=gb.get_data(field_name, attrs)
                 cls._data['page_data']=data
-
         if 'page_action' in cls._conf:
             page_action=cls._conf['page_action']
             getattr(browser, page_action)(job=cls._job)
 
-        ### ------- last
-        post_action=cls._conf['post_action']
-        if post_action:
-            ps=Pythoness()
-            ps.set_job(cls._.job)
-            ps.set_fields(post_action)
-            ps.session(browser)
-            ps.save_data()
 
     @classmethod
     def save_data(cls):
         '''
         '''
         for dict_item in cls._data['page_data']:
-            print(dict_item)
+            print('------------ FINAL DICT',dict_item)
 #            for field_name, values in dict_item.items():
 #                for value in values:
 #                    if not value:continue

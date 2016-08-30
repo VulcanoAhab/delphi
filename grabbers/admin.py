b34425702f73c74cc06a7b42f1419d385676e7bf
VulcanoAhab <vulcano.ahab@gmail.com>
2016-08-30 09:07:43 -0300

    Search field in target admin

--- grabbers/admin.py
+++ grabbers/admin.py
@@ -7,11 +7,17 @@ class SequenceAdmin(admin.ModelAdmin):
     '''
     filter_horizontal=['indexed_grabbers',]
 
+
+class TargetAdmin(admin.ModelAdmin):
+    '''
+    '''
+    search_fields=['field_name',]
+
 admin.site.register(Sequence, SequenceAdmin)
 admin.site.register(Mapper)
 admin.site.register(IndexedGrabber)
 admin.site.register(Grabber)
-admin.site.register(Target)
+admin.site.register(Target,TargetAdmin)
 admin.site.register(Extractor)
 admin.site.register(PostElementAction)
 admin.site.register(ElementAction)

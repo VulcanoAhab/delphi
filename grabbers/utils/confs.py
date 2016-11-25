e97d9a3cd6f097b04dc8163b7c57f25312cfce77
Jeymisson Oliveira <jeymissoncz@gmail.com>
2016-11-25 15:22:44 -0200

    small fix on grabber confs extractors checking

--- grabbers/utils/confs.py
+++ grabbers/utils/confs.py
@@ -28,8 +28,8 @@ class GrabberConf:
             tempDict['name']=target_name
             tempDict['selector_type']=target_selector_type
             work_dict['target']=tempDict
-        extis=grabberObj.extractors.all()
-        if extis.count():
+        if grabberObj.extractors and grabberObj.extractors.all().count():
+	    extis = grabberObj.extractors.all()
             work_dict['extractors']=[e.type for e in extis]
         if grabberObj.element_action:
             work_dict['element_action']=grabberObj.element_action.type

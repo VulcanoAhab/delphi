a813287d686b3359df3b96bd35bfa589cbdd127c
Jeymisson Oliveira <jeymissoncz@gmail.com>
2017-03-22 22:12:27 -0300

    added error view

--- workers/views.py
+++ workers/views.py
@@ -5,9 +5,9 @@ from grabbers.models import Sequence
 from rest_framework import viewsets, generics
 from rest_framework.response import Response
 from workers.serializers import (JobSerializer,
-                                TaskConfigListSerializer,
-                                TaskConfigDetailSerializer,
-                                TaskSerializer)
+                                 TaskConfigListSerializer,
+                                 TaskConfigDetailSerializer,
+                                 TaskSerializer)
 
 
 # Create your views here.
@@ -21,12 +21,12 @@ class JobsViewSet(viewsets.ModelViewSet):
     def retrieve(self, request, pk=None):
         '''
         '''
-        job=Job.objects.get(pk=pk)
-        jobse=JobSerializer(job)
-        full_job=jobse.data
-        task_sample=Task.objects.filter(job=job).first()
-        task_config=task_sample.config
-        task_configse=TaskConfigDetailSerializer(task_config)
+        job = Job.objects.get(pk=pk)
+        jobse = JobSerializer(job)
+        full_job = jobse.data
+        task_sample = Task.objects.filter(job=job).first()
+        task_config = task_sample.config
+        task_configse = TaskConfigDetailSerializer(task_config)
         full_job.update(task_configse.data)
         return Response(full_job)
 
@@ -37,7 +37,7 @@ class TaskConfigsViewSet(viewsets.ModelViewSet):
     """
     queryset = TaskConfig.objects.all().order_by('id')
     serializer_class = TaskConfigListSerializer
-    lookup_field='name'
+    lookup_field = 'name'
 
     def retrieve(self, request, name=None):
         """
